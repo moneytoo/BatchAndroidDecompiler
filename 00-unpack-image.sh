01c4ea756b849a8beb96e1c1e6f373604f00633c
Marcel Dopita <m@rcel.cz>
2017-10-22 11:15:55 +0200

    macOS

--- 00-unpack-image.sh
+++ 00-unpack-image.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-./simg2img.exe system.img system.raw
+./simg2img system.img system.raw
+
+# Windows
 /cygdrive/c/Program\ Files/7-Zip/7z.exe x -osystem system.raw
+
+# macOS
+#mkdir -p mnt/ system/
+#ext4fuse system.raw mnt/
+#cp -r mnt/* system/
+#umount mnt/
